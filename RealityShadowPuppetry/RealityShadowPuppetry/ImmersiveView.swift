47c4902c7a1db96623b90e8b9f9c8fd1e5bbd69a
xander-xuSC <xander-xu@foxmail.com>
2025-06-16 17:09:20 +0800

    clean code;

--- RealityShadowPuppetry/RealityShadowPuppetry/ImmersiveView.swift
+++ RealityShadowPuppetry/RealityShadowPuppetry/ImmersiveView.swift
@@ -7,44 +7,14 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 import MetalPerformanceShaders
 import MetalKit
 
 struct ImmersiveView: View {
-    var textureDescriptor: LowLevelTexture.Descriptor {
-        var desc = LowLevelTexture.Descriptor()
-
-
-        desc.textureType = .type2D
-        desc.arrayLength = 1
-
-
-        desc.width = 2048
-        desc.height = 2048
-        desc.depth = 1
-
-
-        desc.mipmapLevelCount = 1
-        desc.pixelFormat = .bgra8Unorm
-        desc.textureUsage = [.shaderRead, .shaderWrite]
-        desc.swizzle = .init(red: .red, green: .green, blue: .blue, alpha: .alpha)
-
-
-        return desc
-    }
-    var inTexture: MTLTexture {
-        let textureLoader = MTKTextureLoader(device: MTLCreateSystemDefaultDevice()!)
-        let texture: MTLTexture = try! textureLoader.newTexture(name: "panghu", scaleFactor: 1, bundle: nil)
-        return texture
-    }
     var body: some View {
         RealityView { content in
             do {
-                let texture = try LowLevelTexture(descriptor: textureDescriptor)
-                let resource = try TextureResource(from: texture)
-                
-                let planeEntity = try textureEntity(device: MTLCreateSystemDefaultDevice()!)
+                let planeEntity = try textureEntity(device: MTLCreateSystemDefaultDevice()!, imageName: "panghu")
                 content.add(planeEntity)
             } catch {
                 print(error)
@@ -52,27 +22,48 @@ struct ImmersiveView: View {
             
         }
     }
-    
-    func textureEntity(device: MTLDevice) throws -> Entity {
-        // Create the LowLevelTexture and populate it on the GPU.
-        let texture = try LowLevelTexture(descriptor: textureDescriptor)
-        populate(texture: texture, device: device)
+    func createTextureDescriptor(width: Int, height: Int) -> LowLevelTexture.Descriptor {
+        var desc = LowLevelTexture.Descriptor()
 
+        desc.textureType = .type2D
+        desc.arrayLength = 1
+
+        desc.width = width
+        desc.height = height
+        desc.depth = 1
+
+        desc.mipmapLevelCount = 1
+        desc.pixelFormat = .bgra8Unorm
+        desc.textureUsage = [.shaderRead, .shaderWrite]
+        desc.swizzle = .init(red: .red, green: .green, blue: .blue, alpha: .alpha)
+
+        return desc
+    }
+    
+    func textureEntity(device: MTLDevice, imageName: String) throws -> Entity {
+        // Load the input texture.
+        let textureLoader = MTKTextureLoader(device: device)
+        let inTexture: MTLTexture = try textureLoader.newTexture(name: imageName, scaleFactor: 1, bundle: nil)
+        
+        // Create a descriptor for the LowLevelTexture.
+        let textureDescriptor = createTextureDescriptor(width: inTexture.width, height: inTexture.height)
+        // Create the LowLevelTexture and populate it on the GPU.
+        let llt = try LowLevelTexture(descriptor: textureDescriptor)
+        
+        
+        populate(inTexture: inTexture, lowLevelTexture: llt, device: device)
 
         // Create a TextureResource from the LowLevelTexture.
-        let resource = try TextureResource(from: texture)
-
-
+        let resource = try TextureResource(from: llt)
         // Create a material that uses the texture.
         var material = UnlitMaterial(texture: resource)
         material.opacityThreshold = 0.5
-
 
         // Return an entity of a plane which uses the generated texture.
         return ModelEntity(mesh: .generatePlane(width: 1, height: 1), materials: [material])
     }
     
-    func populate(texture: LowLevelTexture, device: MTLDevice) {
+    func populate(inTexture: MTLTexture, lowLevelTexture: LowLevelTexture, device: MTLDevice) {
         // Set up the Metal command queue and compute command encoder,
         // or abort if that fails.
         guard let commandQueue = device.makeCommandQueue(),
@@ -82,9 +73,9 @@ struct ImmersiveView: View {
         
         // Create a MPS filter.
         let blur = MPSImageGaussianBlur(device: device, sigma: 8)
-        let t = texture.replace(using: commandBuffer)
+        let outTexture = lowLevelTexture.replace(using: commandBuffer)
         
-        blur.encode(commandBuffer: commandBuffer, sourceTexture: inTexture, destinationTexture: t)
+        blur.encode(commandBuffer: commandBuffer, sourceTexture: inTexture, destinationTexture: outTexture)
         // The usual Metal enqueue process.
         commandBuffer.commit()
         commandBuffer.waitUntilCompleted()
